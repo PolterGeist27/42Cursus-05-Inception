fefc0a92ff5129bd6a4334c75182c801abed4107
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-12-06 11:27:44 +0000

    mariadb works now

---
+++ Inception/srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This variables should be stored in env
+#DB_NAME=database1
+#DB_USER=user1
+#DB_PASSWORD=abc
+#DB_PASS_ROOT=123
+
+service mariadb start
+
+mariadb -v -u root << EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS_ROOT');
+EOF
+
+sleep 5
+
+service mariadb stop
+
+exec $@
+
