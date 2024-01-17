c73d22d6ebe41f6c466c510855b178dc13e1bee6
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2024-01-17 11:17:46 +0000

    fixed a couple of errors, but like an hydra for each solved two more appeared...

--- Inception/srcs/requirements/wordpress/tools/setup.sh
+++ Inception/srcs/requirements/wordpress/tools/setup.sh
@@ -1,45 +1,40 @@
-#!/bin/bash
-
-WP_URL=diogmart.42.fr
-WP_TITLE=Inception
-WP_ADMIN_USER=root1
-WP_ADMIN_PASSWORD=123
-WP_ADMIN_EMAIL=root1@123.com
-WP_USER=user1
-WP_PASSWORD=abc
-WP_EMAIL=user1@123.com
-WP_ROLE=editor
-
-chown -R www-data:www-data /var/www/inception/
-
-if [ ! -f "/var/www/inception/wp-config.php" ]; then
-   mv /tmp/wp-config.php /var/www/inception/
-fi
-
+# Pause execution for 10 seconds
 sleep 10
 
-wp --allow-root --path="/var/www/inception/" core download || true
+#--url="$WP_URL": Sets the WordPress site URL.
+#--title="$WP_TITLE": Sets the site title.
+#--admin_user="$WP_ADMIN_USER": Sets the admin username.
+#--admin_password="$WP_ADMIN_PASSWORD": Sets the admin password.
+#--admin_email="$WP_ADMIN_EMAIL": Sets the admin email.
+#--skip-email: Skips sending email notifications during installation.
+#--path='/var/www/wordpress': Specifies the installation directory path.
+#--allow-root: Allows running WP-CLI commands as the root user.
 
-if ! wp --allow-root --path="/var/www/inception/" core is-installed;
-then
-    wp  --allow-root --path="/var/www/inception/" core install \
-        --url=$WP_URL \
-        --title=$WP_TITLE \
-        --admin_user=$WP_ADMIN_USER \
-        --admin_password=$WP_ADMIN_PASSWORD \
-        --admin_email=$WP_ADMIN_EMAIL
-fi;
+#"$WP_USER": User's login (username).
+#"$WP_USER_EMAIL": User's email address.
+#--user_pass="$WP_USER_PASSWORD": User's password.
+#--role="$WP_ROLE": Sets the user role to "author."
+#--path='/var/www/wordpress': Specifies the installation directory path.
+#--allow-root: Allows running WP-CLI commands as the root user.
 
-if ! wp --allow-root --path="/var/www/inception/" user get $WP_USER;
-then
-    wp  --allow-root --path="/var/www/inception/" user create \
-        $WP_USER \
-        $WP_EMAIL \
-        --user_pass=$WP_PASSWORD \
-        --role=$WP_ROLE
-fi;
+# Install WordPress
+wp core install --url="$WP_URL" \
+    --title="$WP_TITLE" \
+    --admin_user="$WP_ADMIN_USER" \
+    --admin_password="$WP_ADMIN_PASSWORD" \
+    --admin_email="$WP_ADMIN_EMAIL" \
+    --skip-email \
+    --path='/var/www/wordpress' \
+    --allow-root
 
-wp --allow-root --path="/var/www/inception/" theme install raft --activate 
+# Create a new WordPress user
+wp user create "$WP_USER" "$WP_USER_EMAIL" \
+    --user_pass="$WP_USER_PASSWORD" \
+    --role="$WP_ROLE" \
+    --path='/var/www/wordpress' \
+    --allow-root
 
-exec $@
+# Start PHP FastCGI Process Manager
+# PHP-FPM is responsible for handling PHP requests. 
+/usr/sbin/php-fpm7.4 -F
 
